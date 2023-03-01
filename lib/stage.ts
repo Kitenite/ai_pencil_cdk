48b70bd4d49979f1849c1d5e955a8dc8b5845fee
Kiet Ho <hoakiet98@gmail.com>
2023-02-28 22:21:37 -0600

    Remove unecessary pipline code

--- lib/stage.ts
+++ lib/stage.ts
@@ -1,24 +1,4 @@
-import { Stage, StageProps } from 'aws-cdk-lib';
-import { Construct } from "constructs";
-import { AsynStack } from './async-stack';
-import { APP_NAME } from './constants';
-
 export enum PipelineStages {
-    BETA = 'Beta',
-    PROD = 'Prod'
-  }
-  
-export interface PipelineStageProps extends StageProps {
-    stage: PipelineStages;
-}
-
-export class PipelineStage extends Stage {
-    
-    constructor(scope: Construct, id: string, props: PipelineStageProps) {
-      super(scope, id, props);
-  
-      const asyncStack = new AsynStack(this, `${APP_NAME}${props.stage}AsyncStack`, {
-        stage: props.stage
-      });
-    }
+  BETA = 'Beta',
+  PROD = 'Prod'
 }
